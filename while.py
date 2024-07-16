f30f626c9c2277c25f6cb183cfda7efbab232bff
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-16 19:30:33 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,12 @@
+# loop - repeats a set of  code - while,for
+letter = " "
+while not letter.isalpha():
+    letter = input("Enter a alphabet: ")
+print("You have entered " + letter)
+
+#output
+'''
+Enter a alphabet: a
+You have entered a
+
+'''
